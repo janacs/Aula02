26cefe52569f04200f9d7ee5596d94812e564a88
Janderson <jandersonsouza_phn@hotmail.com>
2025-02-26 09:54:04 -0300

    segunda aula de Python

---
+++ app.py
@@ -0,0 +1,21 @@
+# Objetos mutáveis e imutáveis em Python
+
+verdade = "PyThOn,é,InCrÍvEl"
+
+# lower () = método que retorna a variável em letras minúsculas
+print(verdade.lower())
+
+# upper () = método que retorna a variável em letras maíusculas
+print(verdade.upper())
+
+# capitalize () = método que retorna a variável com a inicial maiúscula
+print(verdade.capitalize())
+
+# replace () = método que retorna a variável com a substituíção dela, porém tem a palavra que ser idêntica
+print(verdade.replace("PyThOn" , "Docker"))
+
+# split () = método que separa a string em arrays (listas)
+print(verdade.split(","))
+
+# join () = 
+print(verdade.join(["Mentira", "Verdade" , "Dúvida"]))
